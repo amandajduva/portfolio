94836f3ae7f57955ac6a335827bb8a569b55356f
Amanda Duva <amanda.j.duva@gmail.com>
2023-09-01 12:41:12 -0400

    updated Contact.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
             <p>
                 Hello, my name is Amanda Duva and I am a web developer. I graduated from University of Central Florida with a Bachelor of Science in English Language Arts Education. I started off my professional life as a high school teacher. After three years as an educator, I attended a coding bootcamp through UCF to earn a certificate and to become the web devloper that I am today!
             </p>
-            <img src={updatedPhoto} alt="profile"/>
+            <img src={updatedPhoto} alt="profile" />
         </div>
     );
 }
