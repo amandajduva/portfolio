3017f259c164844ea17c11f11730c6ddee7d432d
Amanda Duva <amanda.j.duva@gmail.com>
2023-09-06 16:16:30 -0400

    updated Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,3 +1,28 @@
 export default function Resume() {
-    return "Resume";
+    return (
+        <div className="resume">
+            <a href="../../assets/portfolio-resume.pdf">⭐️ Download My Resume ⭐️</a>
+            <h4>My proficiencies:</h4>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Git</li>
+                <li>JavaScript</li>
+                <li>Web, Third-Party, and Server-Side APIs</li>
+                <li>JQuery</li>
+                <li>Bootstrap</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>React.js</li>
+                <li>MySQL</li>
+                <li>Object-Relational Mapping</li>
+                <li>MongoDB</li>
+                <li>Model-View-Controller</li>
+                <li>MERN Stack</li>
+                <li>Progressive Web Applications</li>
+                <li>Object Oriented Programming</li>
+                <li>Agile Development</li>
+            </ul>
+        </div>
+    );
 }
